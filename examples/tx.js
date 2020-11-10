7a80962f1a882f4c453f581d615c01138d2b5bf6
pchambers <peter.chambers1@gmail.com>
2020-11-10 15:18:59 -0500

    bugfix add debug to tx.js

--- examples/tx.js
+++ examples/tx.js
@@ -6,7 +6,7 @@ const {exec} = require('child_process');
 var SX128x = require('../lib/sx128x');
 const regMasks = require('../lib/regMasks');
 
-var options = {
+let options = {
     modParams : { modParam1: regMasks.LORA_SF_7, modParam2:regMasks.LORA_BW_400, modParam3:regMasks.LORA_CR_4_5},
     pktParams : {pktParam1: 0x08, pktParam2:0x00, pktParam3:0x0F, pktParam4:0x00, pktParam5:0x00, pktParam6:0x00, pktParam7:0x00},
     txParams : {power: 0x1F, rampTime:0xE0},
