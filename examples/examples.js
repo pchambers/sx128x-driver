a88ed31d4b0a06cf98ca89bffefcbe1139f43978
pchambers <peter.chambers1@gmail.com>
2020-11-10 10:29:29 -0500

    updated dio1 to busy pin

---
+++ examples/examples.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const util = require('util');
+const {exec} = require('child_process');
+
+var SX128x = require('../lib/sx128x');
+const regMasks = require('../lib/regMasks');
+
+//Create new radio instance with default options
+var options = {
+    modParams : { modParam1: LORA_SF_7, modParam2:LORA_BW_400, modParam3:LORA_CR_4_5},
+    pktParams : {pktParam1: 0x08, pktParam2:0x00, pktParam3:0x0F, pktParam4:0x00, pktParam5:0x00, pktParam6:0x00, pktParam7:0x00},
+    txParams : {power: 0x1F, rampTime:0xE0},
+    irqMask : {irqMask:[0x40,0x23]},
+    dioMask : {dio1Mask:[0x00,0x01], dio2Mask:[0x00,0x02], dio3Mask:[0x40,0x20]},
+    spiBus : 0,
+    spiDevice : 0,
+    resetPin : 17,
+    busyPin : 18,
+    packetType : regMasks.PACKET_TYPE_LORA,
+    regulatorMode : 0x01,
+    ranging : False,
+    status : 0,
+    rfFreq : regMasks.FREQ_24GHZ,
+    autoFS : False
+}
+
+var radio = new SX128x({options});
--- examples/tx.js
+++ examples/tx.js
@@ -2,38 +2,3 @@ var SX128x = require('../lib/sx128x');
 
 //Create new radio instance with default options
 var radio = new SX128x({});
-async function send(){
-        let count =0;
-
-        try{
-            await radio.open();
-        }
-        catch(err){
-            console.log(err);
-        }
-        while(true) {
-            //send a message every second
-            try {
-                await radio.write(new Buffer('hello ' + count++));
-                console.log('successfully sent');;
-            } catch(err){
-                console.log(err);
-            }
-            await util.promisify(setTimeout)(1000);
-        }
-}
-
-send();
-
-process.on('SIGINT', async function(){
-    //close the _device
-    try{
-        await radio.close();
-    }catch(err){
-        console.log('close failure: ' + err);
-        process.exit();
-    }
-
-    console.log('success');
-    process.exit();
-})
