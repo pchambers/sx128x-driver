6b0c21d73358c653fe5027056d2c788abfaaa0c1
pchambers <peter.chambers@gmail.com>
2020-12-10 15:26:19 -0500

    updated examples.js test

--- examples/examples.js
+++ examples/examples.js
@@ -1,24 +1,27 @@
+const express = require('express');
+const app = express();
+const util = require('util');
+const {exec} = require('child_process');
 
 var SX128x = require('../lib/sx128x');
-const regMasks = require('../lib/regMasks');
 
-var options = {
-    modParams : { modParam1: regMasks.LORA_SF_7, modParam2:regMasks.LORA_BW_400, modParam3:regMasks.LORA_CR_4_5},
-    pktParams : {pktParam1: 0x08, pktParam2:0x00, pktParam3:0x0F, pktParam4:0x00, pktParam5:0x00, pktParam6:0x00, pktParam7:0x00},
+let options = {
+    modParams : { modParam1: SX128x.LORA_SF_5, modParam2:SX128x.LORA_BW_1600, modParam3:SX128x.LORA_CR_4_5},
+    pktParams : {pktParam1: SX128x.LORA_PBLE_12_SYMB, pktParam2:SX128x.EXPLICIT_HEADER, pktParam3:0xFF, pktParam4:SX128x.LORA_CRC_DISABLE, pktParam5:SX128x.LORA_IQ_STD, pktParam6:0x00, pktParam7:0x00},
     txParams : {power: 0x1F, rampTime:0xE0},
-    irqMask : {irqMask:[0x40,0x23]},
-    dioMask : {dio1Mask:[0x00,0x01], dio2Mask:[0x00,0x02], dio3Mask:[0x40,0x20]},
+    irqMask : {irqMask:[0x00,0x00]},
+    dioMask : {dio1Mask:[0x00,0x00], dio2Mask:[0x00,00], dio3Mask:[0x00,0x00]},
     spiBus : 0,
     spiDevice : 0,
     resetPin : 17,
     busyPin : 18,
-    packetType : regMasks.PACKET_TYPE_LORA,
+    packetType : SX128x.PACKET_TYPE_LORA,
     regulatorMode : 0x01,
     ranging : false,
     status : 0,
-    rfFreq : regMasks.FREQ_24GHZ,
+    rfFreq : SX128x.FREQ_24GHZ,
     autoFS : false
-}
+};
 
-var radio = new SX128x({options});
+var radio = new SX128x({});//spiBus: 0,spiDevice : 0
 radio.checkDevice();
