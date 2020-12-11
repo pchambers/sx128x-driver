4b3f20161db35c1a31efe2b1c014759994f793d6
pchambers <peter.chambers@gmail.com>
2020-12-11 15:34:17 -0500

    fixed setTxParam missing arguments

--- examples/tx.js
+++ examples/tx.js
@@ -8,7 +8,7 @@ var SX128x = require('../lib/sx128x');
 let options = {
     modParams : { modParam1: SX128x.LORA_SF_5, modParam2:SX128x.LORA_BW_1600, modParam3:SX128x.LORA_CR_4_5},
     pktParams : {pktParam1: SX128x.LORA_PBLE_12_SYMB, pktParam2:SX128x.EXPLICIT_HEADER, pktParam3:0xFF, pktParam4:SX128x.LORA_CRC_DISABLE, pktParam5:SX128x.LORA_IQ_STD, pktParam6:0x00, pktParam7:0x00},
-    txParams : {power: 0x1F, rampTime:0xE0},
+    txParams : {power: 0x1F, rampTime:SX128x.RADIO_RAMP_40_US},
     irqMask : {irqMask:[0x00,0x00]},
     dioMask : {dio1Mask:[0x00,0x00], dio2Mask:[0x00,00], dio3Mask:[0x00,0x00]},
     spiBus : 0,
