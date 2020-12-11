79a119a9cd55aaf8eecb6c3a9952648c3a4285e8
pchambers <peter.chambers1@gmail.com>
2020-12-11 17:31:08 -0500

    added a write/read buffer test to examples/example.js -- debug

--- examples/examples.js
+++ examples/examples.js
@@ -30,11 +30,11 @@ async function testCheck(){
     //await radio._config();
     await radio.checkDevice();
     await radio._checkBusy();
-    
+
     let msg = Buffer.from('hello')
     await radio.writeBuffer(msg,0x00);
     console.log(msg);
-    let rtn = await radio.readBuffer(0x00, msg.length);
+    let rtn = await radio.readBuffer(msg.length, msg.length);
     console.log(rtn);
 };
 
