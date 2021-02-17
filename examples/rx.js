bdc9fb7f36a369e8a5f5f06cb2bd66ad6bca887e
pchambers <>
2021-02-17 09:19:29 -0500

    fixed typo that was setting txParam to 40us when it should have been 04 us

--- examples/rx.js
+++ examples/rx.js
@@ -10,7 +10,7 @@ let options = {
     rxBaseAddress: 0x00,
     modParams : { modParam1: SX128x.LORA_SF_6, modParam2:SX128x.LORA_BW_1600, modParam3:SX128x.LORA_CR_4_5},
     pktParams : {pktParam1: SX128x.LORA_PBLE_12_SYMB, pktParam2:SX128x.EXPLICIT_HEADER, pktParam3:0xFF, pktParam4:SX128x.LORA_CRC_DISABLE, pktParam5:SX128x.LORA_IQ_STD, pktParam6:0x00, pktParam7:0x00},
-    txParams : {power: 0x1F, rampTime:SX128x.RADIO_RAMP_40_US},
+    txParams : {power: 0x1F, rampTime:SX128x.RADIO_RAMP_04_US},
     irqMask : {irqMask:SX128x.IRQ_RADIO_ALL},
     dioMask : {dio1Mask: 0x0001, dio2Mask:0x0000, dio3Mask:0x0000},
     spiBus : 0,
