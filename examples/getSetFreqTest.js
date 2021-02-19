bcee86df19fab9c63ab8a513d1ce4f44c890c8e2
pchambers <>
2021-02-19 15:18:31 -0500

    added getSetFreq test

---
+++ examples/getSetFreqTest.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const util = require('util');
+const {exec} = require('child_process');
+
+var SX128x = require('../lib/sx128x');
+var options = require('./options');
+
+var radio = new SX128x(options);
+
+async function getSetTest(){
+    radio.listen = true;
+
+    await radio.open();
+    await radio.checkDevice();
+    await radio._checkBusy();
+
+    var input = 0xBC1994;
+    radio.setRFFreq(input);
+    var returnFreq = radio._getFreqInt();
+    console.log(returnFreq);
+};
+
+getSetTest();
+
+process.on('SIGNINT', async function () {
+    //close the devices
+    try {
+        await radio.close();
+    } catch (err) {
+        console.log(err);
+        process.exit();
+    }
+});
