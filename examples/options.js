0749ded53e9036aae3ceaaa17832f17c4b614ec2
pchambers <>
2021-02-20 20:08:33 -0500

    added debug to readRegister.

--- examples/options.js
+++ examples/options.js
@@ -4,7 +4,7 @@ var options = {
     txBaseAddress: 0x00,
     rxBaseAddress: 0x00,
     modParams : { spreadingFactor: SX128x.LORA_SF_7, bandwidth:SX128x.LORA_BW_400,codingRate:SX128x.LORA_CR_4_5},
-    pktParams : {pktParam1: SX128x.LORA_PBLE_12_SYMB, pktParam2:SX128x.LORA_PKT_VARIABLE_LENGTH, pktParam3:0xFF, pktParam4:SX128x.LORA_CRC_ENABLE, pktParam5:SX128x.LORA_IQ_STD, pktParam6:0x00, pktParam7:0x00},
+    pktParams : {pktParam1: SX128x.LORA_PBLE_12_SYMB, pktParam2:SX128x.LORA_PKT_VARIABLE_LENGTH, pktParam3:SX128x.RADIO_RX_MATCH_SYNCWORD_OFF, pktParam4:SX128x.LORA_CRC_ENABLE, pktParam5:SX128x.LORA_IQ_STD, pktParam6:0x00, pktParam7:0x00},
     txParams : {power: 0x1F, rampTime:SX128x.RADIO_RAMP_04_US},
     irqMask : {irqMask:SX128x.IRQ_RADIO_ALL},
     dioMask : {dio1Mask: (SX128x.IRQ_TX_DONE + SX128x.IRQ_RX_DONE + SX128x.IRQ_RX_TX_TIMEOUT), dio2Mask:0x0000, dio3Mask:0x0000},
